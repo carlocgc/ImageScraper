00c848e2a93ccf78bf4df967c8d4f828ae8e01d6
carlocgc <carlocgc@hotmail.com>
2023-04-03 22:09:06 +0100

    https requests working

--- ImageScraper/main.cpp
+++ ImageScraper/main.cpp
@@ -2,24 +2,40 @@
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
+#include <filesystem>
+#include <string>
 
 
 int main(int argc, char* argv[])
 {
+    const std::filesystem::path exe_path = std::filesystem::path(argv[0]).parent_path();
+    const std::filesystem::path cert_path = exe_path / "curl-ca-bundle.crt";
+    const std::string user_agent = "carlocgc";
 
-    // Initialize curlpp
+    std::string subreddit = "programming";
+    std::string endpoint = "https://www.reddit.com/r/" + subreddit + "/hot.json";
     curlpp::Cleanup cleanup;
     curlpp::Easy request;
-
-    // Set the URL to request
-    request.setOpt(new curlpp::options::Url("http://google.com"));
-
-    // Send
     std::ostringstream response;
+
+    // Set the URL to retrieve
+    request.setOpt(new curlpp::options::Url(endpoint));
+
+    // Follow redirects if any
+    request.setOpt(new curlpp::options::FollowLocation(true));
+
+    // Set user agent
+    request.setOpt(new curlpp::options::UserAgent(user_agent));
+
+    // Set the output stream for the response
     request.setOpt(new curlpp::options::WriteStream(&response));
+
+    request.setOpt(new curlpp::options::CaInfo(cert_path.generic_string()));
+
+    // Perform the request and get the response
     request.perform();
 
-    // Print response
+    // Print the response
     std::cout << response.str() << std::endl;
 
     return 0;
