c99ae0101c9d0d2cb196e354d6423e252a8fca3d
carlocgc <carlocgc@hotmail.com>
2023-04-06 09:23:33 +0100

    Added comment to main

--- ImageScraper/src/main.cpp
+++ ImageScraper/src/main.cpp
@@ -1,3 +1,14 @@
+/*
+Project Name: ImageScraper
+Author: Carlo Mongiello
+Date: 2023
+
+Description:
+The purpose of this program is to scrape media content from a website URL,including social media feeds, and store it locally.
+The program allows for asynchronous downloading of all media at the specified URL,with a graphical user interface (GUI) that allows the user to input the URL and configure options.
+The program also displays log output to the user.
+*/
+
 #include <sstream>
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
@@ -8,7 +19,6 @@
 #include "Logger.h"
 #include "RedditRequest.h"
 #include "DownloadRequest.h"
-
 
 int main( int argc, char* argv[ ] )
 {
