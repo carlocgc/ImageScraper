3ec7540977b91e4c9790791f1597dd844a0ac8d3
carlocgc <carlocgc@hotmail.com>
2023-04-06 09:24:21 +0100

    formatting

--- ImageScraper/src/main.cpp
+++ ImageScraper/src/main.cpp
@@ -4,8 +4,8 @@ Author: Carlo Mongiello
 Date: 2023
 
 Description:
-The purpose of this program is to scrape media content from a website URL,including social media feeds, and store it locally.
-The program allows for asynchronous downloading of all media at the specified URL,with a graphical user interface (GUI) that allows the user to input the URL and configure options.
+The purpose of this program is to scrape media content from a website URL, including social media feeds, and store it locally.
+The program allows for asynchronous downloading of all media at the specified URL, with a graphical user interface (GUI) that allows the user to input the URL and configure options.
 The program also displays log output to the user.
 */
 
