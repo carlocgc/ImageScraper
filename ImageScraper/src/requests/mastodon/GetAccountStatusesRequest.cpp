b69a8c35fb195a27c781f5502b540c77764b7541
carlocgc <carlocgc@hotmail.com>
2026-04-24 17:50:52 +0100

    Deduplicate Mastodon request helpers

--- ImageScraper/src/requests/mastodon/GetAccountStatusesRequest.cpp
+++ ImageScraper/src/requests/mastodon/GetAccountStatusesRequest.cpp
@@ -2,48 +2,12 @@
 
 #include "network/CurlHttpClient.h"
 #include "network/RetryHttpClient.h"
-#include "utils/DownloadUtils.h"
+#include "requests/mastodon/MastodonRequestHelpers.h"
 #include "utils/MastodonUtils.h"
 #include "utils/StringUtils.h"
 #include "log/Logger.h"
 
 #include <utility>
-#include <vector>
-
-namespace
-{
-    std::string CreateEncodedQueryParamString( const std::vector<ImageScraper::QueryParam>& params )
-    {
-        std::string paramString{ };
-        if( !params.empty( ) )
-        {
-            paramString += '?';
-        }
-
-        int paramCount = 0;
-        for( const ImageScraper::QueryParam& param : params )
-        {
-            if( paramCount++ > 0 )
-            {
-                paramString += '&';
-            }
-
-            paramString += ImageScraper::StringUtils::UrlEncode( param.m_Key );
-            paramString += '=';
-            paramString += ImageScraper::StringUtils::UrlEncode( param.m_Value );
-        }
-
-        return paramString;
-    }
-
-    ImageScraper::RequestResult MakeBadRequest( const std::string& message )
-    {
-        ImageScraper::RequestResult result{ };
-        result.m_Error.m_ErrorCode = ImageScraper::ResponseErrorCode::BadRequest;
-        result.m_Error.m_ErrorString = message;
-        return result;
-    }
-}
 
 const std::string ImageScraper::Mastodon::GetAccountStatusesRequest::s_PathPrefix = "/api/v1/accounts/";
 const std::string ImageScraper::Mastodon::GetAccountStatusesRequest::s_PathSuffix = "/statuses";
@@ -66,50 +30,34 @@ ImageScraper::RequestResult ImageScraper::Mastodon::GetAccountStatusesRequest::P
     if( instanceUrl.empty( ) )
     {
         LogDebug( "[%s] Mastodon::GetAccountStatusesRequest missing or invalid instance URL.", __FUNCTION__ );
-        return MakeBadRequest( "Missing or invalid Mastodon instance URL" );
+        return RequestHelpers::MakeBadRequest( "Missing or invalid Mastodon instance URL" );
     }
 
     if( options.m_ResourceId.empty( ) )
     {
         LogDebug( "[%s] Mastodon::GetAccountStatusesRequest missing account ID.", __FUNCTION__ );
-        return MakeBadRequest( "Missing Mastodon account ID" );
+        return RequestHelpers::MakeBadRequest( "Missing Mastodon account ID" );
     }
-
-    RequestResult result{ };
 
     HttpRequest request{ };
     request.m_Url = instanceUrl
         + s_PathPrefix
         + StringUtils::UrlEncode( options.m_ResourceId )
         + s_PathSuffix
-        + CreateEncodedQueryParamString( options.m_QueryParams );
+        + RequestHelpers::CreateEncodedQueryParamString( options.m_QueryParams );
     request.m_UserAgent = options.m_UserAgent;
     request.m_CaBundle = options.m_CaBundle;
-
-    if( !options.m_AccessToken.empty( ) )
-    {
-        request.m_Headers = { "Authorization: Bearer " + options.m_AccessToken };
-    }
+    RequestHelpers::ApplyBearerAuthHeader( request, options.m_AccessToken );
 
     LogDebug( "[%s] Mastodon::GetAccountStatusesRequest, URL: %s", __FUNCTION__, request.m_Url.c_str( ) );
 
     const HttpResponse response = m_HttpClient->Get( request );
-    if( !response.m_Success )
-    {
-        result.m_Error.m_ErrorCode = ResponseErrorCodefromInt( response.m_StatusCode );
-        result.m_Error.m_ErrorString = response.m_Error;
-        LogDebug( "[%s] Mastodon::GetAccountStatusesRequest failed! %s", __FUNCTION__, result.m_Error.m_ErrorString.c_str( ) );
-        return result;
-    }
-
-    result.m_Response = response.m_Body;
-    if( DownloadHelpers::IsMastodonResponseError( result ) )
+    const RequestResult result = RequestHelpers::CreateResultFromResponse( response );
+    if( !result.m_Success )
     {
         LogDebug( "[%s] Mastodon::GetAccountStatusesRequest failed! %s", __FUNCTION__, result.m_Error.m_ErrorString.c_str( ) );
         return result;
     }
-
-    result.m_Success = true;
 
     LogDebug( "[%s] Mastodon::GetAccountStatusesRequest complete!", __FUNCTION__ );
     return result;
--- ImageScraper/src/requests/mastodon/SearchAccountsRequest.cpp
+++ ImageScraper/src/requests/mastodon/SearchAccountsRequest.cpp
@@ -2,57 +2,12 @@
 
 #include "network/CurlHttpClient.h"
 #include "network/RetryHttpClient.h"
-#include "utils/DownloadUtils.h"
+#include "requests/mastodon/MastodonRequestHelpers.h"
 #include "utils/MastodonUtils.h"
-#include "utils/StringUtils.h"
 #include "log/Logger.h"
 
-#include <algorithm>
 #include <utility>
 #include <vector>
-
-namespace
-{
-    bool HasQueryParam( const std::vector<ImageScraper::QueryParam>& params, const std::string& key )
-    {
-        return std::any_of( params.begin( ), params.end( ), [ &key ]( const ImageScraper::QueryParam& param )
-            {
-                return param.m_Key == key;
-            } );
-    }
-
-    std::string CreateEncodedQueryParamString( const std::vector<ImageScraper::QueryParam>& params )
-    {
-        std::string paramString{ };
-        if( !params.empty( ) )
-        {
-            paramString += '?';
-        }
-
-        int paramCount = 0;
-        for( const ImageScraper::QueryParam& param : params )
-        {
-            if( paramCount++ > 0 )
-            {
-                paramString += '&';
-            }
-
-            paramString += ImageScraper::StringUtils::UrlEncode( param.m_Key );
-            paramString += '=';
-            paramString += ImageScraper::StringUtils::UrlEncode( param.m_Value );
-        }
-
-        return paramString;
-    }
-
-    ImageScraper::RequestResult MakeBadRequest( const std::string& message )
-    {
-        ImageScraper::RequestResult result{ };
-        result.m_Error.m_ErrorCode = ImageScraper::ResponseErrorCode::BadRequest;
-        result.m_Error.m_ErrorString = message;
-        return result;
-    }
-}
 
 const std::string ImageScraper::Mastodon::SearchAccountsRequest::s_Path = "/api/v2/search";
 
@@ -74,51 +29,36 @@ ImageScraper::RequestResult ImageScraper::Mastodon::SearchAccountsRequest::Perfo
     if( instanceUrl.empty( ) )
     {
         LogDebug( "[%s] Mastodon::SearchAccountsRequest missing or invalid instance URL.", __FUNCTION__ );
-        return MakeBadRequest( "Missing or invalid Mastodon instance URL" );
+        return RequestHelpers::MakeBadRequest( "Missing or invalid Mastodon instance URL" );
     }
 
-    if( !HasQueryParam( options.m_QueryParams, "q" ) )
+    if( !RequestHelpers::HasQueryParam( options.m_QueryParams, "q" ) )
     {
         LogDebug( "[%s] Mastodon::SearchAccountsRequest missing query parameter: q", __FUNCTION__ );
-        return MakeBadRequest( "Missing Mastodon account search query" );
+        return RequestHelpers::MakeBadRequest( "Missing Mastodon account search query" );
     }
 
-    RequestResult result{ };
     std::vector<QueryParam> queryParams = options.m_QueryParams;
-    if( !HasQueryParam( queryParams, "type" ) )
+    if( !RequestHelpers::HasQueryParam( queryParams, "type" ) )
     {
         queryParams.push_back( { "type", "accounts" } );
     }
 
     HttpRequest request{ };
-    request.m_Url = instanceUrl + s_Path + CreateEncodedQueryParamString( queryParams );
+    request.m_Url = instanceUrl + s_Path + RequestHelpers::CreateEncodedQueryParamString( queryParams );
     request.m_UserAgent = options.m_UserAgent;
     request.m_CaBundle = options.m_CaBundle;
-
-    if( !options.m_AccessToken.empty( ) )
-    {
-        request.m_Headers = { "Authorization: Bearer " + options.m_AccessToken };
-    }
+    RequestHelpers::ApplyBearerAuthHeader( request, options.m_AccessToken );
 
     LogDebug( "[%s] Mastodon::SearchAccountsRequest, URL: %s", __FUNCTION__, request.m_Url.c_str( ) );
 
     const HttpResponse response = m_HttpClient->Get( request );
-    if( !response.m_Success )
-    {
-        result.m_Error.m_ErrorCode = ResponseErrorCodefromInt( response.m_StatusCode );
-        result.m_Error.m_ErrorString = response.m_Error;
-        LogDebug( "[%s] Mastodon::SearchAccountsRequest failed! %s", __FUNCTION__, result.m_Error.m_ErrorString.c_str( ) );
-        return result;
-    }
-
-    result.m_Response = response.m_Body;
-    if( DownloadHelpers::IsMastodonResponseError( result ) )
+    const RequestResult result = RequestHelpers::CreateResultFromResponse( response );
+    if( !result.m_Success )
     {
         LogDebug( "[%s] Mastodon::SearchAccountsRequest failed! %s", __FUNCTION__, result.m_Error.m_ErrorString.c_str( ) );
         return result;
     }
-
-    result.m_Success = true;
 
     LogDebug( "[%s] Mastodon::SearchAccountsRequest complete!", __FUNCTION__ );
     return result;
